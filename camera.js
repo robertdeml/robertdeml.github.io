2766e7ff85c61e284a58669876821275d785be5e
Robert Deml <rdeml31@gmail.com>
2024-09-13 20:03:17 -0400

    remove old code

--- camera.js
+++ camera.js
@@ -1,64 +1,66 @@
 var imageCapture;
 
-async function onCaptureMap() {
-    await onGetUserMediaButtonClick();
-}
-const videoOptions = { facingMode:{ exact: "environment"} };
-const videoOptionLatptop = { video: true};
-
-// get a list of available cameras
-async function onGetUserMediaButtonClick() {
-    navigator.mediaDevices.getUserMedia({ video: videoOptionLatptop } )
-        .then(async mediaStream => {
-
-            const track = mediaStream.getVideoTracks()[0];
-            imageCapture = new ImageCapture(track);
-
-            onTakePhotoButtonClick();
-
-            setTimeout( () => {
-            mediaStream.getTracks().forEach(function(track) {
-                track.stop();
-              });
-            }, 5000);
-        })
-        .catch(error => console.log(error));
+const debug = (str) => {
+  const span = document.createElement('span');
+  span.innerHTML = str;
+  document.querySelector('#debug').appendChild(span);
 }
 
-function onTakePhotoButtonClick() {
-    return imageCapture.takePhoto()
-        .then(blob => createImageBitmap(blob))
-        .then(imageBitmap => {
-            const canvas = document.querySelector('#image');
-            canvas.style.width = imageBitmap.width + "px";
-            canvas.style.height = imageBitmap.height + "px";
+// async function onCaptureMap() {
+//     await onGetUserMediaButtonClick();
+// }
+// const videoOptions = { facingMode:{ exact: "environment"} };
+// const videoOptionLatptop = { video: true};
 
-            drawCanvas(canvas, imageBitmap);
-        })
-        .catch(error => console.log(error));
-}
+// // get a list of available cameras
+// async function onGetUserMediaButtonClick() {
+//     navigator.mediaDevices.getUserMedia({ video: videoOptionLatptop } )
+//         .then(async mediaStream => {
+
+//             const track = mediaStream.getVideoTracks()[0];
+//             imageCapture = new ImageCapture(track);
+
+//             onTakePhotoButtonClick();
+
+//             setTimeout( () => {
+//             mediaStream.getTracks().forEach(function(track) {
+//                 track.stop();
+//               });
+//             }, 5000);
+//         })
+//         .catch(error => console.log(error));
+// }
+
+// function onTakePhotoButtonClick() {
+//     return imageCapture.takePhoto()
+//         .then(blob => createImageBitmap(blob))
+//         .then(imageBitmap => {
+//             const canvas = document.querySelector('#image');
+//             canvas.style.width = imageBitmap.width + "px";
+//             canvas.style.height = imageBitmap.height + "px";
+
+//             drawCanvas(canvas, imageBitmap);
+//         })
+//         .catch(error => console.log(error));
+// }
 
 /* Utils */
 
-function drawCanvas(canvas, img) {
-    canvas.width = getComputedStyle(canvas).width.split('px')[0];
-    canvas.height = getComputedStyle(canvas).height.split('px')[0];
-    let ratio = Math.min(canvas.width / img.width, canvas.height / img.height);
-    let x = (canvas.width - img.width * ratio) / 2;
-    let y = (canvas.height - img.height * ratio) / 2;
-    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
-    canvas.getContext('2d').drawImage(img, 0, 0, img.width, img.height,
-        x, y, img.width * ratio, img.height * ratio);
-}
+// function drawCanvas(canvas, img) {
+//     canvas.width = getComputedStyle(canvas).width.split('px')[0];
+//     canvas.height = getComputedStyle(canvas).height.split('px')[0];
+//     let ratio = Math.min(canvas.width / img.width, canvas.height / img.height);
+//     let x = (canvas.width - img.width * ratio) / 2;
+//     let y = (canvas.height - img.height * ratio) / 2;
+//     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+//     canvas.getContext('2d').drawImage(img, 0, 0, img.width, img.height,
+//         x, y, img.width * ratio, img.height * ratio);
+// }
 
 // document.getElementById('captureMap').addEventListener('click', onCaptureMap);
 
 const fileInput = document.querySelector('input[type="file"]');
-const debug = (str) => {
-    const span = document.createElement('span');
-    span.innerHTML = str;
-    document.querySelector('#debug').appendChild(span);
-}
+
 
 debug("attaching listener. ");
 fileInput.addEventListener('change', (event) => {
