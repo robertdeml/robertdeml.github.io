62bfe8259a146b9d2f3e90d04a6514f1ac3612a9
robertdeml <rdeml31@gmail.com>
2026-06-21 08:13:01 -0400

    chore: bump version to 1.0.16

--- src/version.ts
+++ src/version.ts
@@ -6,7 +6,7 @@
  * significant releases.
  * ============================================================ */
 
-export const VERSION = "1.0.15";
+export const VERSION = "1.0.16";
 
 /** Fetches the compiled version.js and reloads if the version
  *  differs from the currently loaded one.  Silently ignores
