d158df73e7fbb34d51e2dae4ca64ba213e8198cf
robertdeml <rdeml31@gmail.com>
2026-06-22 22:31:24 -0400

    chore: version bump

--- src/version.ts
+++ src/version.ts
@@ -5,7 +5,7 @@
  * Bump MAJOR or MINOR manually for significant releases.
  * ============================================================ */
 
-export const VERSION = "1.0.16";
+export const VERSION = "1.0.17";
 
 /** Fetches the compiled version.js and reloads if the version
  *  differs from the currently loaded one.  Silently ignores
